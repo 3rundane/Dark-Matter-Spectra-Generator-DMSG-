0bb17de4decbf1c890990a522598556e4dcc6669
3rundane <44929615+3rundane@users.noreply.github.com>
2019-08-12 21:40:03 -0700

    Add files via upload

---
+++ DMSG/DMSG_computePoissonUncertainty.py
@@ -0,0 +1,19 @@
+import Generation,Bin
+import numpy as np
+import math
+
+#This function is meant to simply compute the standard deviation of a poisson distribution whose mean is given as
+# as a list of arguments (signalCount).
+# This returns a list of standard deviations computed by iterating through signalCount and using its elements as inputs for
+# poisson distributions to draw from. It is then square-rooted to obtain the sample standard deviation. Note I may have
+# done the statistics wrong here.
+def computeSigma(signalCount):
+    lengthOfList = len(signalCount)
+    yourSigma = [0]*lengthOfList
+    for i in range(lengthOfList):
+        if(signalCount[i]==0):
+            yourSigma[i]=1
+        else:
+            yourSigma[i] = math.sqrt(np.random.poisson(signalCount[i]))
+            # yourSigma[i] = math.sqrt(signalCount[i])
+    return yourSigma
---
+++ Example Scripts/exa_plotBackgroundandSignals.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from pylab import *
+import matplotlib.pyplot as plt
+import scipy.optimize as optimize
+import Generation,Start,Bin
+# This file is a script used to compute plots of generated background spectra, generated mock signal events
+# and background spectra + generated mock signal events.
+# This file is organized as follows: first we use the Start "wrapper" to create a new Generation object for plotting uses.
+# Second, we plot 3 different figures. Figure(1) plots in loglog scale the background events created by DracoBackground.py
+# Figure(2) plots the DDM signal events generated with xscale in log form.
+# Figure(3) plots the bin by bin sum of DDM signal events with the background events with xscale in log form.
+draco=Start.start(48,10,120,48,135)
+lengthArray = len(draco.getSignalBinCount())
+
+lengthBG = len(draco.getBackgroundBinCount())
+
+energyLength = len(draco.getBinEnergy())
+
+totalcountlist = [0]*len(draco.getSignalBinCount())
+for i in range(lengthArray):
+    totalcountlist[i] = draco.getBackgroundBinCount()[i] + draco.getSignalBinCount()[i] #This has not been unittested yet.
+
+plt.figure(1)
+#plt.plot(draco.getBinEnergy(),draco.getBackgroundBinCount(),'g^')
+plt.loglog(draco.getBinEnergy(),draco.getBackgroundBinCount(),'g.')
+#plt.loglog(draco.getBinEnergy(),draco.getSignalBinCount(),'g^')
+# plt.plot(draco.getBinEnergy(),draco.getSignalBinCount(),'r^')
+plt.title('Draco Background Events')
+plt.ylabel('total Events per bin width (#events/MeV)')
+plt.xlabel('Bin energy(MeV)')
+
+
+
+
+figure(2)
+plt.plot(draco.getBinEnergy(),draco.getSignalBinCount(),'b.')
+plt.title('DDM Signal Events')
+plt.ylabel('total Events per bin width (#events/MeV)')
+plt.xlabel('Bin energy(MeV)')
+plt.xscale('log')
+
+figure(3)
+plt.plot(draco.getBinEnergy(),totalcountlist,'k.')
+plt.title('Total Events')
+plt.ylabel('total Events per bin width (#events/MeV)')
+plt.xlabel('Bin energy(MeV)')
+plt.xscale('log')
+plt.show()
